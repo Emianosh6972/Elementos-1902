b5d6b3b11da0e7f0622c0ef503bf9e42686af3dc
ogallardo <ogallardo@AIRAM_OEGB.aristeia.com.mx>
2019-02-19 21:42:54 -0600

    Conexion a mongodb, dependencias agregadas y conexion exitosa

--- src/main/java/org/unitec/elementos/ElementosApplication.java
+++ src/main/java/org/unitec/elementos/ElementosApplication.java
@@ -1,14 +1,27 @@
 package org.unitec.elementos;
 
+import com.sun.corba.se.impl.activation.CommandHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ElementosApplication {
+public class ElementosApplication implements CommandLineRunner
+{
+    @Autowired
+    RepoUsuario repoU;
+    
+    public static void main(String[] args) 
+    {
+        SpringApplication.run(ElementosApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(ElementosApplication.class, args);
-	}
-
+    @Override
+    public void run(String... args) throws Exception 
+    {
+        // Aqui se hallaran todas las pruebas sin necesidad de una interfaz gráfica
+        repoU.save(new Usuario(1, "Emiliano"));
+    }
 }
 
--- src/main/java/org/unitec/elementos/Usuario.java
+++ src/main/java/org/unitec/elementos/Usuario.java
@@ -1,43 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.unitec.elementos;
 
-/**
- *
- * @author T-107
- */
-public class Usuario {
+import org.springframework.data.annotation.Id;
+
+public class Usuario 
+{
+    @Id
     private Integer id;
     private String nombre;
     private Localizacion localizacion;
 
-    public Usuario() {
-    }
+    public Usuario() { }
 
-    public Localizacion getLocalizacion() {
-        return localizacion;
-    }
-
-    public void setLocalizacion(Localizacion localizacion) {
-        this.localizacion = localizacion;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
+    public Usuario(Integer id, String nombre) 
+    {
         this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
+    public Localizacion getLocalizacion() { return localizacion; }
+
+    public void setLocalizacion(Localizacion localizacion) { this.localizacion = localizacion; }
+
+    public Integer getId() { return id; }
+
+    public void setId(Integer id) { this.id = id; }
+
+    public String getNombre() { return nombre; }
+
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
 }
