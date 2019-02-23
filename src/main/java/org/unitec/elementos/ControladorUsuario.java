0a9f13e66644fb541d6b1e5e23db5f11803190a9
ogallardo <ogallardo@AIRAM_OEGB.aristeia.com.mx>
2019-02-22 21:39:37 -0600

    Conocimiento de capa controlador, creacion de metodos "Guardar como..."

--- src/main/java/org/unitec/elementos/ControladorUsuario.java
+++ src/main/java/org/unitec/elementos/ControladorUsuario.java
@@ -1,22 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.unitec.elementos;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-/**
- *
- * @author T-107
- */
 
 @RestController
 @RequestMapping("/api")
 @CrossOrigin
-public class ControladorUsuario {
+
+public class ControladorUsuario 
+{
+    @Autowired RepoUsuario repoU;
+    // Metodo 1 -> Guardar usuario : save()
+        @PostMapping("/usuario")
+        Estatus guardar(@RequestBody String json) throws Exception
+        {
+            ObjectMapper maper = new ObjectMapper();
+            
+            Usuario u = maper.readValue(json, Usuario.class);
+            
+            repoU.save(u);
+            
+            Estatus e = new Estatus ("Usuario guardado con exito!!!", true);
+            
+            return e;
+        }
     
 }
